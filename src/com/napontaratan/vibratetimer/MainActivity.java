882494bfec3b3062eec91f60a1848d77caad8b5f
whatisthis123 <daniel9618@gmail.com>
2014-02-01 15:26:30 -0800

    ListView adapter working

--- src/com/napontaratan/vibratetimer/MainActivity.java
+++ src/com/napontaratan/vibratetimer/MainActivity.java
@@ -1,4 +1,3 @@
-
 package com.napontaratan.vibratetimer;
 
 import java.util.*;
@@ -20,21 +19,19 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	
 	private List<VibrateTimer> vibrateTimers;
+	private String[] days = new String[]{"Su","Mo","Tu","We","Th","Fr","Sa"};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-//		if// load from the database != null)
-//			vibrateTimers = ; // loading from database;
-//		else
-//			vibrateTimers = new ArrayList<VibrateTimer>();
 		
 		ListView listOfVibrates = (ListView) findViewById(R.id.vibrates);
 		if(vibrateTimers == null)
 			vibrateTimers = new ArrayList<VibrateTimer>();
-		listOfVibrates.setAdapter(new VibrateArrayAdapter(this.getApplicationContext(), R.layout.vibrate, vibrateTimers));
+		vibrateTimers.add(new VibrateTimer(null,null,false,null,0));
+		listOfVibrates.setAdapter(new VibrateArrayAdapter(this, R.layout.vibrate, vibrateTimers));
 	}
 
 	@Override
@@ -46,14 +43,15 @@ public class MainActivity extends Activity {
 
 
 	private class VibrateArrayAdapter extends ArrayAdapter<VibrateTimer> {
-
 		//		    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 		private List<VibrateTimer> vibrateTimers;
 		private int resourceId;
+		private Context context;
 		
 		public VibrateArrayAdapter(Context context, int customViewResourceId,
 				List<VibrateTimer> vibrateTimers) {
 			super(context, customViewResourceId, vibrateTimers);
+			this.context = context;
 			this.vibrateTimers = vibrateTimers;
 			resourceId = customViewResourceId;
 		}
@@ -70,21 +68,24 @@ public class MainActivity extends Activity {
 			return vibrateTimers.get(arg0);
 		}
 
-//		@Override
-//		public long getItemId(int arg0) {
-//			// TODO Auto-generated method stub
-//		}
-
 		@Override
-		public View getView(int arg0, View arg1, ViewGroup arg2) {
+		public View getView(int position, View convertView, ViewGroup parent) {
 			// TODO Auto-generated method stub
-			View view = null;
-			if(arg1 == null){
-				LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				view = inflater.inflate(resourceId, arg2);
-						
-			}
-			return view;
+			LayoutInflater inflater = (LayoutInflater) context
+			        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View itemView = inflater.inflate(resourceId, parent, false);
+			VibrateTimer timer = vibrateTimers.get(position);
+			String startTime = timer.getStartTime().getTime().toString();
+			String endTime = timer.getEndTime().getTime().toString();
+			
+			TextView timeStart = (TextView) itemView.findViewById(R.id.time_start);
+			timeStart.setText("Starting Time");
+			TextView timeEnd = (TextView) itemView.findViewById(R.id.time_end);
+			timeEnd.setText("Ending time");
+			TextView day = (TextView) itemView.findViewById(R.id.day);
+			// note: cast int position to string bcuz not doing so throws string resource not found err
+			day.setText(Integer.toString(position));
+			return itemView;	
 		}
 
 
