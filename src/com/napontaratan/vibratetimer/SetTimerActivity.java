bf6335a18fb3fef34fe4c59e7521150829bdb9ea
BryanTai <redhatpieman@gmail.com>
2014-02-08 16:28:41 -0800

    can now check for oldTimer stuff

--- src/com/napontaratan/vibratetimer/SetTimerActivity.java
+++ src/com/napontaratan/vibratetimer/SetTimerActivity.java
@@ -1,5 +1,6 @@
 package com.napontaratan.vibratetimer;
 
+import java.io.IOException;
 import java.util.Calendar;
 
 import com.napontaratan.vibratetimer.controller.VibrateTimerController;
@@ -15,15 +16,50 @@ import android.widget.ToggleButton;
 
 public class SetTimerActivity extends Activity {
 
+	VibrateTimer oldTimer = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_set_timer);
 		
 		Bundle extras = getIntent().getExtras();
+		
 		if (extras != null) {
-		    byte[] value = extras.getByteArray("")
+		    try {
+				oldTimer = (VibrateTimer)VibrateTimer.deserialize(extras.getByteArray("selected_timer"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
+		/**
+		 *  If editing an existing VibrateTimer, set all the values of the UI
+		 */
+		if(oldTimer != null){
+			TimePicker start = (TimePicker) findViewById(R.id.startTimePicker);
+			TimePicker end   = (TimePicker) findViewById(R.id.endTimePicker);
+			Calendar startTime = oldTimer.getStartTime();
+			Calendar endTime   = oldTimer.getEndTime();
+			start.setCurrentHour(startTime.get(Calendar.HOUR));
+			start.setCurrentMinute(startTime.get(Calendar.MINUTE));
+			end.setCurrentHour(endTime.get(Calendar.HOUR));
+			end.setCurrentMinute(endTime.get(Calendar.MINUTE));
+			
+			int[] buttonIds = new int[]
+					{R.id.Sunday, R.id.Monday, R.id.Tuesday, R.id.Wednesday, R.id.Thursday, R.id.Friday, R.id.Saturday};
+			for(int x = 0; x<7;x++){
+				ToggleButton tb = (ToggleButton) findViewById(buttonIds[x]);
+				tb.setChecked(oldTimer.getDays()[x]);
+			}
+			
+		}
+		
+		
 		final Button saveButton = (Button) findViewById(R.id.SaveButton);
 		final Button cancelButton = (Button) findViewById(R.id.CancelButton);
 
@@ -37,10 +73,15 @@ public class SetTimerActivity extends Activity {
 				Calendar end = generateCalendar(R.id.endTimePicker);
 				boolean[] days = generateDays();
 				int id = VibrateTimerController.generateNextId(SetTimerActivity.this);
-
 				VibrateTimer vt = new VibrateTimer(start, end, days, id);
+				
 				VibrateTimerController vtc = new VibrateTimerController(SetTimerActivity.this);
 				vtc.setAlarm(vt, SetTimerActivity.this);
+				if(oldTimer != null){
+					vtc.cancelAlarm(oldTimer, SetTimerActivity.this);
+				}
+				
+				//GO BACK
 			}
 		});
 		
@@ -50,6 +91,8 @@ public class SetTimerActivity extends Activity {
 			public void onClick(View v) {
 				// TODO 
 				System.out.println("Clicked on Close");
+				//GO BACK
+				
 				
 			}
 		});
